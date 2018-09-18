6c7900ce906e0807fccafa37a35f13cd34834b51
AoyuLiu <liuaoyu2012@yeah.net>
2018-09-18 12:13:17 +0000

    前台商品列表支持按类目筛选

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\Category;
 use App\Exceptions\InvalidRequestException;
 use Illuminate\Http\Request;
 use App\Models\Product;
@@ -28,6 +28,17 @@ class ProductsController extends Controller
             });
         }
 
+        if ($request->input('category_id')&& $category = Category::find($request->input('category_id'))) {
+            
+            if ($category->is_directory) {
+                $builder->whereHas('category',function($query) use($category){
+                    $query->where('path','like',$category->path.$category->id.'-%');
+                });
+            } else {
+                $builder->where('category_id',$category->id);
+            }
+        }
+
         // 是否有提交 order 参数，如果有就赋值给 $order 变量
         // order 参数用来控制商品的排序规则
         if ($order = $request->input('order', '')) {
@@ -49,6 +60,8 @@ class ProductsController extends Controller
                 'search' => $search,
                 'order'  => $order,
             ],
+            //isset($category)? $category: null
+            'category'=>$category ?? null,
         ]);
     }
 
