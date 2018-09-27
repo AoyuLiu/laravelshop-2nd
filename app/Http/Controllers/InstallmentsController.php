b826ec850d6259618cdbe2826104b2faa88d5a4f
AoyuLiu <liuaoyu2012@yeah.net>
2018-09-27 09:59:10 +0000

    前台分期付款详情页

--- app/Http/Controllers/InstallmentsController.php
+++ app/Http/Controllers/InstallmentsController.php
@@ -15,4 +15,15 @@ class InstallmentsController extends Controller
 
     	return view('installments.index',['installments'=> $installments]);
     }
+
+    public function show(Installment $installment)
+    {
+    	$items = $installment->items()->OrderBy('sequence')->get();
+    	return view('installments.show',[
+    		'installment' => $installment,
+    		'items'       => $items,
+    		'nextItem'    => $items->where('paid_at',null)->first(),
+
+    	]);
+    }
 }
